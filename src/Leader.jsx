f02506362745ab78de6920c603acf9d338dc08a4
JoshuaDDF <144938706+joshuaddf@users.noreply.github.com>
2024-01-08 16:00:36 +0000

    Update Leader.jsx

--- src/Leader.jsx
+++ src/Leader.jsx
@@ -13,7 +13,7 @@ const Leader = () => {
 
         <div className="flex items-centerjustify-center lg:absolute lg:top-[10rem] lg:left-[34rem] lg:w-[40rem] lg:h-[25rem] lg:px-[3rem] lg:pt-[3rem] lg:text-start lg:pl-[6rem] bg-White mx-[1.7rem] flex-col text-center">
             <h2 className='mt-[2.5rem] mb-[1rem] text-[2rem] font-Joe  lg:text-[3rem] '>THE LEADER IN INTERACTIVE VR</h2>
-            <p className='font-Alata text-Dark-Gray'>Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformes businesses through digital experiences that bind to their brand.</p>
+            <p className='font-Alata text-Dark-Gray'>Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.</p>
         </div>
     </section>
   )
